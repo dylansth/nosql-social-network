a205ef6c15b084648d0a52788585397ab7ef5c6e
Simba <dylan.sthilaire@proton.me>
2023-06-30 15:44:20 -0700

    user model

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const { Schema, Types, model } = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema(
     {
@@ -8,5 +8,36 @@ const userSchema = new Schema(
             trim: true,
             required: "Username Required",
         },
+
+        email: {
+            type: String,
+            required: "Email Required",
+            unique: true,
+            match: [/.+@.+\..+/],
+        },
+
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: "Thought",
+        }],
+
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false, 
     }
-)
+);
+
+userSchema.virtual("friendCount").get(function () {
+    return this.friends.length;
+});
+
+const User = model ("User", userSchema);
+
+module.exports = User;

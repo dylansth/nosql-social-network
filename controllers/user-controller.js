9a172be3484f0c61e93717f75d588a3ab4473558
Simba <dylan.sthilaire@proton.me>
2023-07-03 10:10:18 -0700

    user-route, user-controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -37,6 +37,36 @@ const userController = {
           })
           .catch((err) => res.status(500).json(err));
       },
+
+      createUser(req, res) {
+        User.create(req.body)
+        .then((userData) => res.json (userData))
+        .catch((err) => res.status(500).json(err));
+      },
+
+      updateUser(req, res) {
+        User.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        .then((userData) => {
+          if (!userData) {
+            res.status(404).json({ message: "No user found with this id!" });
+            return;
+          }
+          res.json(userData);
+        })
+        .catch((err) => res.status(500).json(err));
+      },
+
+      deleteUser(req, res) {
+        User.findByIdAndDelete(req.params.id)
+          .then((userData) => {
+            if (!userData) {
+              res.status(404).json({ message: "No user found with this id!" });
+              return;
+            }
+            res.json({ message: "User deleted successfully!" });
+          })
+          .catch((err) => res.status(500).json(err));
+      },
 }
 
 module.exports = userController
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -2,10 +2,19 @@ const router = require("express").Router();
 const {
     getAllUsers,
     getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
 } = require ("../../controllers/user-controller")
 
 router.route("/").get(getAllUsers);
 
-router.route("/:id").get(getUserById);
+router.route("/:userId").get(getUserById);
+
+router.route("/").post(createUser);
+
+router.route("/:id").put(updateUser);
+
+router.route("/:id").delete(deleteUser);
 
 module.exports = router
